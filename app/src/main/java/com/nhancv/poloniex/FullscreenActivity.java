4e3d901b8822e68b1df3fe948c153a1c064bcb1e
Nhan Cao <nhancv92@gmail.com>
2017-05-31 14:01:20 +0700

    update webview

--- app/src/main/java/com/nhancv/poloniex/FullscreenActivity.java
+++ app/src/main/java/com/nhancv/poloniex/FullscreenActivity.java
@@ -1,14 +1,22 @@
 package com.nhancv.poloniex;
 
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.net.http.SslError;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.CookieManager;
+import android.webkit.PermissionRequest;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class FullscreenActivity extends AppCompatActivity {
     private WebView webView;
+    private String url = "https://poloniex.com";
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
@@ -17,13 +25,53 @@ public class FullscreenActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_fullscreen);
         webView = (WebView) findViewById(R.id.webView);
+
         // Enable Javascript
         WebSettings webSettings = webView.getSettings();
+        //Enable Javascript;
         webSettings.setJavaScriptEnabled(true);
+        //Use WideViewport and Zoom out if there is no viewport defined
+        webSettings.setUseWideViewPort(true);
+        webSettings.setLoadWithOverviewMode(true);
+        //Enable pinch to zoom without the zoom buttons
+        webSettings.setBuiltInZoomControls(true);
+        //Allow use of Local Storage
+        webSettings.setDomStorageEnabled(true);
+        //Hide the zoom controls for HONEYCOMB+
+        webSettings.setDisplayZoomControls(false);
 
-        webView.loadUrl("https://poloniex.com");
+        //AppRTC requires third party cookies to work
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            CookieManager cookieManager = CookieManager.getInstance();
+            cookieManager.setAcceptThirdPartyCookies(webView, true);
+        }
+
+        //Hide scrollbar
+        webView.setVerticalScrollBarEnabled(false);
+        webView.loadUrl(url);
         // Force links and redirects to open in the WebView instead of in a browser
-        webView.setWebViewClient(new WebViewClient());
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                handler.proceed();
+            }
+        });
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                runOnUiThread(new Runnable() {
+                    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+                    @Override
+                    public void run() {
+                        if (request.getOrigin().toString().contains(url)) {
+                            request.grant(request.getResources());
+                        } else {
+                            request.deny();
+                        }
+                    }
+                });
+            }
+        });
 
     }
 
